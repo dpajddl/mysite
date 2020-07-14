7907b54c1364e5948c4d0a15c81670424ad1c030
Yewon Shin <970won@gmail.com>
2020-07-14 17:50:28 +0900

    first

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e^r+2h4cm44ekv%)a=^_yqyu24p#2+*3@qgl-uf+kb90*%14g-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
